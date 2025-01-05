edb5e6824ea3cdb9394182c5f0585981334d4803
Eduardo Tejada <105448798+Davidpedo123@users.noreply.github.com>
2025-01-05 16:10:53 -0400

    clear middleware.py

--- src/app/middleware.py
+++ src/app/middleware.py
@@ -3,9 +3,9 @@ from starlette.responses import RedirectResponse
 
 class RedirectToAPIMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request, call_next):
-        # Si la ruta no empieza con '/api', redirige a '/api'
+        
         if not request.url.path.startswith("/api/instituciones"):
             return RedirectResponse(url=f"/api/instituciones")
-        # Si la ruta ya tiene el prefijo '/api', continúa con el flujo normal
+        
         response = await call_next(request)
         return response
